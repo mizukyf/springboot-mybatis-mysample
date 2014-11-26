ceb9c898cdec688a8d5bc0e2814860af4bbee9a5
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-11-27 03:49:13 +0900

    /helloのコントローラを削除（/hello?name=*と重複するため）

--- src/main/java/sample/controller/SampleController.java
+++ src/main/java/sample/controller/SampleController.java
@@ -17,24 +17,11 @@ public class SampleController {
 	@Autowired
 	private SampleService sampleService;
 	
-	/**
-	 * 最も単純なハンドラ・メソッドの例.
-	 * 戻り値でビュー名を指定し、引数として受け取った{@link Model}参照にモデル属性（≒テンプレート変数）を設定している。
-	 * ビュー名は"(クラスパス)/templates/..."配下のHTMLファイルのパスとして処理される。
-	 * テンプレート側で存在しないモデル属性を参照しようとするとエラーが発生するので注意。
-	 * @param model モデル
-	 * @return ビュー名
-	 */
-    @RequestMapping("/hello")
-    public String hello(Model model) {
-    	model.addAttribute("message", "hello world!");
-    	return "hello";
-    }
-
     /**
      * パス変数を使用したハンドラ・メソッドの例.
-     * {@link #hello(Model)}と同じく戻り値としてビュー名を返すタイプのハンドラ。
-     * ただしモデル属性として設定するメッセージの生成には、
+	 * 戻り値でビュー名を指定し、引数として受け取った{@link Model}参照にモデル属性（≒テンプレート変数）を設定している。
+	 * ビュー名は"(クラスパス)/templates/..."配下のHTMLファイルのパスとして処理される。
+     * モデル属性として設定するメッセージの生成には、
      * リクエスト・パスの断片から値を抽出する「パス変数」と呼ばれるテクニックを使用している。
      * @param name パス変数
      * @param model モデル
